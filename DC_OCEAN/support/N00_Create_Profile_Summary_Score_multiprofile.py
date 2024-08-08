8f1c8d225d7f57db4c9ef29e5d0c2f5cfe300301
Bec <rebecca.cowley@csiro.au>
2024-08-09 09:35:52 +1000

    Finalise the multiprofile code to create the npz files and concatenate the outputs of all the multiprofile files into one npz file.

--- DC_OCEAN/support/N00_Create_Profile_Summary_Score_multiprofile.py
+++ DC_OCEAN/support/N00_Create_Profile_Summary_Score_multiprofile.py
@@ -64,27 +64,40 @@ def validate_path(input_path):
 
     return True
 
-#Read NetCDF files and pre-processing metadata and secondary processing data. Retain numerical metadata and convert string metadata into numerical values by using the ASCII code table and then summing these ASCII code values of each letter to obtain a single value.
-def read_netCDF_formatted_PSS_series(inputpath, outputpath):
 
+def find_dataset_item(search_string):
+    # assign a number based on the dataset name
+    datasets_dict = {'bod': 1, 'bottle': 1, 'ocean station': 1, 'osd': 1, 'low-resolution': 1,
+                     'low resolution': 1, 'ctd': 2, 'xctd': 2,
+                     'mbt': 3, 'mechanica': 3, 'mb': 3, 'xbt': 4, 'xb': 4, 'expendable': 4, 'sur': 5,
+                     'surface': 5, 'apb': 6, 'autonomous': 6, 'animal': 6, 'mrb': 7, 'moored': 7, 'tao': 7,
+                     'pfl': 8, 'argo': 8, 'profiling': 8, 'drb': 9, 'drifting': 9,
+                     'towed': 10, 'uor': 10, 'undulating': 10, 'gld': 11, 'glider': 11,
+                     'dbt': 12, 'std': 13, 'microbt': 14}
+    matches = [value for key, value in datasets_dict.items() if key in search_string.lower()]
+    return matches[0] if matches else 0
+
+
+# Read NetCDF files and pre-processing metadata and secondary processing data. Retain numerical metadata and convert string metadata into numerical values by using the ASCII code table and then summing these ASCII code values of each letter to obtain a single value.
+def read_netCDF_formatted_PSS_series_sub(inputpath, outputpath):
     warnings.filterwarnings("ignore")
 
     # Path where the netCDF files are stored
     inputpath = os.path.normpath(os.path.abspath(inputpath))
     print(inputpath)
 
-
     # Get all file names in the directory that are not directories themselves
     filenames = [f for f in os.listdir(inputpath) if os.path.isfile(os.path.join(inputpath, f))]
 
     # print(filenames)
     # Initialize data structures
-    meta_names = ['WOD_id', 'CODA_id', 'Access_no', 'dataset', 'lat', 'lon', 'datetime', 'Temperature_Instrument',
+    meta_names = ['filename', 'WOD_id', 'CODA_id', 'Access_no', 'dataset', 'lat', 'lon', 'datetime',
+                  'Temperature_Instrument',
                   'Recorder', 'needs_z_fix', 'depth_number', 'maximum_depth', 'hasTemp', 'hasSalinity', 'hasOxygen',
                   'hasChlorophyll', 'country', 'WMO_ID', 'dbase_orig', 'Project', 'Platform',
                   'ocean_vehicle', 'Institute', 'WOD_cruise_identifier', 'sum_temp', 'sum_salinity', 'sum_depth',
                   'std_depth', 'std_temp', 'std_salinity', 'corr_temp_depth', 'corr_sal_depth']
-    # create an empty dataframe PSS_series with meta_names as headers
+    # create an empty dataframe PSS_series_sub with meta_names as headers
     PSS_series = pd.DataFrame(columns=meta_names)
 
     # number of files:
@@ -92,7 +105,7 @@ def read_netCDF_formatted_PSS_series(inputpath, outputpath):
 
     # Process each file
     for idx, filename in enumerate(filenames):
-        print(f"Processing file {idx+1}/{n_files}: {filename}")
+        print(f"Processing file {idx + 1}/{n_files}: {filename}")
         file_path = os.path.join(inputpath, filename)
 
         # Open the netCDF file as xarray dataset
@@ -101,6 +114,12 @@ def read_netCDF_formatted_PSS_series(inputpath, outputpath):
         except Exception as e:
             print(f"Failed to open {filename}: {str(e)}")
             continue
+
+        # set up empty dataframe for this file
+        PSS_series_sub = pd.DataFrame(columns=meta_names)
+
+        # add the filename
+        PSS_series_sub['filename'] = filename
 
         # number of profiles is number of casts
         n_prof = len(f['cast'].values)
@@ -112,17 +131,18 @@ def read_netCDF_formatted_PSS_series(inputpath, outputpath):
         depth = np.where((depth > 12000) | (depth < -10), np.nan, depth)
 
         # Calculate number of depth measurements
-        PSS_series['depth_number'] = np.count_nonzero(~np.isnan(depth), axis=1)  # Using len() since depth is a numpy array
+        PSS_series_sub['depth_number'] = np.count_nonzero(~np.isnan(depth),
+                                                          axis=1)  # Using len() since depth is a numpy array
 
         # Determine maximum depth, taking care only to consider valid (non-NaN) entries
         if np.any(~np.isnan(depth)):
-            PSS_series['maximum_depth'] = np.nanmax(depth, axis=1)
+            PSS_series_sub['maximum_depth'] = np.nanmax(depth, axis=1)
         else:
-            PSS_series['maximum_depth'] = np.nan
+            PSS_series_sub['maximum_depth'] = np.nan
 
         # Compute the sum and standard deviation of depth, rounding to four decimal places
-        PSS_series['sum_depth'] = np.round(np.nansum(depth, axis=1), 4)
-        PSS_series['std_depth'] = np.round(np.nanstd(depth, axis =1), 4)
+        PSS_series_sub['sum_depth'] = np.round(np.nansum(depth, axis=1), 4)
+        PSS_series_sub['std_depth'] = np.round(np.nanstd(depth, axis=1), 4)
 
         # Read 'Temperature' data
         if 'Temperature' in f:
@@ -134,26 +154,26 @@ def read_netCDF_formatted_PSS_series(inputpath, outputpath):
             # Filter out NaN values for further processing
             mask = np.isnan(temp)
             depth2 = depth
-            depth2[mask] = np.nan # Assuming 'depth' is already defined and processed similarly
+            depth2[mask] = np.nan  # Assuming 'depth' is already defined and processed similarly
 
             # Check if there are valid temperature readings
-            PSS_series['hasTemp'] = np.any(~np.isnan(temp), axis =1)
+            PSS_series_sub['hasTemp'] = np.any(~np.isnan(temp), axis=1)
 
             # Compute the sum, standard deviation, and correlation coefficient, if applicable
-            PSS_series['sum_temp'] = np.round(np.nansum(temp, axis=1), 5)
-            PSS_series['std_temp'] = np.round(np.nanstd(temp, axis=1), 5)
+            PSS_series_sub['sum_temp'] = np.round(np.nansum(temp, axis=1), 5)
+            PSS_series_sub['std_temp'] = np.round(np.nanstd(temp, axis=1), 5)
             # Calculate correlation if both arrays have non-NaN data and at least two data points
             for i in range(temp.shape[0]):
                 if len(~np.isnan(temp[i])) >= 2:
                     mask = ~np.isnan(temp[i]) & ~np.isnan(depth2[i])
                     if np.sum(mask) > 1:  # Ensure there are at least two points to correlate
-                        PSS_series['corr_temp_depth'][i] = np.corrcoef(temp[i, mask], depth2[i, mask])[0, 1]
+                        PSS_series_sub['corr_temp_depth'][i] = np.corrcoef(temp[i, mask], depth2[i, mask])[0, 1]
 
             # Handle edge cases explicitly
-            PSS_series['sum_temp'] = PSS_series['sum_temp'].replace(0, np.nan)
-            PSS_series['std_temp'] = PSS_series['std_temp'].replace(0, np.nan)
+            PSS_series_sub['sum_temp'] = PSS_series_sub['sum_temp'].replace(0, np.nan)
+            PSS_series_sub['std_temp'] = PSS_series_sub['std_temp'].replace(0, np.nan)
 
-       # Read 'Salinity' data
+        # Read 'Salinity' data
         if 'Salinity' in f:
             sal = f['Salinity'].values
 
@@ -166,129 +186,65 @@ def read_netCDF_formatted_PSS_series(inputpath, outputpath):
             depth2[mask] = np.nan  # Assuming 'depth' is already defined and processed similarly
 
             # Check if there are valid salinity readings
-            PSS_series['hasSalinity'] = np.any(~np.isnan(sal), axis=1)
+            PSS_series_sub['hasSalinity'] = np.any(~np.isnan(sal), axis=1)
 
             # Compute the sum, standard deviation, and correlation coefficient, if applicable
-            PSS_series['sum_salinity'] = np.round(np.nansum(sal, axis=1), 5)
-            PSS_series['std_salinity'] = np.round(np.nanstd(sal, axis=1), 5)
+            PSS_series_sub['sum_salinity'] = np.round(np.nansum(sal, axis=1), 5)
+            PSS_series_sub['std_salinity'] = np.round(np.nanstd(sal, axis=1), 5)
             # Calculate correlation if both arrays have non-NaN data and at least two data points
             for i in range(sal.shape[0]):
                 if len(~np.isnan(sal[i])) >= 2:
                     mask = ~np.isnan(sal[i]) & ~np.isnan(depth2[i])
                     if np.sum(mask) > 1:  # Ensure there are at least two points to correlate
-                        PSS_series['corr_sal_depth'][i] = np.corrcoef(sal[i, mask], depth2[i, mask])[0, 1]
+                        PSS_series_sub['corr_sal_depth'][i] = np.corrcoef(sal[i, mask], depth2[i, mask])[0, 1]
 
             # Handle edge cases explicitly
-            PSS_series['sum_salinity'] = PSS_series['sum_salinity'].replace(0, np.nan)
-            PSS_series['std_salinity'] = PSS_series['std_salinity'].replace(0, np.nan)
+            PSS_series_sub['sum_salinity'] = PSS_series_sub['sum_salinity'].replace(0, np.nan)
+            PSS_series_sub['std_salinity'] = PSS_series_sub['std_salinity'].replace(0, np.nan)
 
         # loop over some of the variables where we just need to know if they exist or not
         for var in ['Oxygen', 'Chlorophyll']:
             if var in f:
-                PSS_series[f'has{var}'] = np.any(~np.isnan(f[var].values), axis=1)
+                PSS_series_sub[f'has{var}'] = np.any(~np.isnan(f[var].values), axis=1)
 
         # Try to read various attributes and variables
-        for var in ['WMO_id', 'lat', 'lon', 'Access_no']:
+        for var in ['WMO_id', 'lat', 'lon', 'Access_no', 'WOD_id']:
             if var in f.variables:
-                PSS_series[var] = np.float64(f[var].values)
+                PSS_series_sub[var] = np.float64(f[var].values)
 
         # Read and process date and time
         time_var = f['time'].values
-        PSS_series['datetime'] = nc.num2date(time_var, f['time'].attrs['units'])
+        PSS_series_sub['datetime'] = nc.num2date(time_var, f['time'].attrs['units'])
 
-    # Loop over variables for ascii sum
+        # Loop over variables for ascii sum
         for var in ['CODA_id', 'country', 'Temperature_Instrument', 'needs_z_fix', 'Recorder', 'dbase_orig',
                     'Project', 'Platform', 'WOD_cruise_identifier', 'Institute']:
             if var in f.variables:
                 var_char = np.array([x.decode('utf-8') for x in f[var].values], dtype=str)
             else:
                 var_char = np.array(['' for _ in range(n_prof)], dtype=str)
-            # loop over all the profiles and convert the country_name, needs_z_fix and probe_type to ASCII sum
-            for i in range(n_prof):
-                    if var_char[i]:
-                        ASCII_sum = sum(ord(char) for char in var_char[i] if char != ' ')
-                        PSS_series[var][i] = ASCII_sum
+            # convert the country_name, needs_z_fix and probe_type etc to ASCII sum
+            ASCII_sums = np.array([sum(ord(char) for char in string if char != ' ') for string in var_char])
+            PSS_series_sub[var] = ASCII_sums
 
-        try:
-            dataset_name = f.variables['dataset'][:]
-            dataset_name = bytes(dataset_name[~dataset_name.mask]).decode('ascii')         
-            # dataset_name = ''.join(chr(x) for x in f.variables['dataset'][:]).strip()
-            dataset_name=dataset_name.lower()
-            if any(sub in dataset_name for sub in ['bod', 'bottle', 'ocean station', 'osd', 'low-resolution', 'low resolution']):
-                dataset_id = 1
-            elif any(sub in dataset_name for sub in ['towed', 'uor', 'undulating']):
-                dataset_id = 10
-            elif any(sub in dataset_name for sub in ['ctd', 'xctd']):
-                dataset_id = 2
-            elif any(sub in dataset_name for sub in ['mbt', 'mechanica', 'mb']):
-                dataset_id = 3
-            elif any(sub in dataset_name for sub in ['xbt', 'xb', 'expendable']):
-                dataset_id = 4
-            elif 'sur' in dataset_name or 'surface' in dataset_name:
-                dataset_id = 5
-            elif any(sub in dataset_name for sub in ['apb', 'autonomous', 'animal']):
-                dataset_id = 6
-            elif any(sub in dataset_name for sub in ['mrb', 'moored', 'tao']):
-                dataset_id = 7
-            elif any(sub in dataset_name for sub in ['pfl', 'argo', 'profiling']):
-                dataset_id = 8
-            elif 'drb' in dataset_name or 'drifting' in dataset_name:
-                dataset_id = 9
-            elif 'gld' in dataset_name or 'glider' in dataset_name:
-                dataset_id = 11
-            elif 'dbt' in dataset_name:
-                dataset_id = 12
-            elif 'std' in dataset_name:
-                dataset_id = 13
-            elif 'microbt' in dataset_name:
-                dataset_id = 14
-            else:
-                dataset_id = np.nan
-        except:
-            dataset_id = np.nan
+        if 'dataset' in f.variables:
+            dataset_name = np.array([x.decode('utf-8') for x in f['dataset'].values], dtype=str)
+        else:
+            dataset_name = np.array(['' for _ in range(n_prof)], dtype=str)
+        # find the matching value from the dictionary
+        dataset_names = np.array([find_dataset_item(item) for item in dataset_name])
+        PSS_series_sub['dataset'] = dataset_names
 
-
-        # Store the processed data
-        ######  please make sure the 'position (order)' of each variables are consistent with the order of meta_names
-        # txt[idx][0]=str(filename)
-        txt[idx][8]=str(probe_type)
-        txt[idx][9]=str(recorder)
-        txt[idx][18]=str(country_name)
-        txt[idx][21]=str(dbase_orig)
-        txt[idx][22]=str(project_name)
-        txt[idx][23]=str(platform)
-        txt[idx][24]=str(ocean_vehicle)
-        txt[idx][25]=str(institute)
-        txt[idx][26]=str(wod_cruise_identifier)
-        strings_columns_order=[8,9,18,21,22,23,24,25,26]
-
-        PSS_series[idx, 0:8]=[wod_unique_id, accession_number, dataset_id, latitude, longitude, year, month, day]
-        PSS_series[idx, 10:18]=[hour, minute, depth_number, maximum_depth, hasTemp, hasSalinity, hasOxygen, hasChlorophyll]
-        PSS_series[idx, 19:21]=[GMT_time, WMO_id]
-        PSS_series[idx, 27:35]=[sum_temp, sum_sal, sum_depth, std_depth, std_temp, std_sal, cor_temp_depth, cor_sal_depth]
-
+        # append the PSS_series_sub to the PSS_series
+        PSS_series = PSS_series.append(PSS_series_sub, ignore_index=True)
 
         # Close the netCDF file
         f.close()
 
+    # Delete WOD_unique_id column
+    PSS_series = PSS_series.drop(columns=['WOD_id'])
 
-    # Delete WOD_unique_id
-    PSS_series= PSS_series[:, 1:]
-    txt = [row[1:] for row in txt]
-    del meta_names[0]   #Delete WOD_unique_id
-
-
-    # Converts the string to the ASCILL code and sums
-    variables_index_to_process = [x - 1 for x in strings_columns_order]
-    for i in range(n_prof):
-        for j in variables_index_to_process:
-            if j < len(txt[i]): 
-                # sum of all ACILL for each string varaible
-                ASCII_sum = sum(ord(char) for char in txt[i][j] if char != ' ')
-                PSS_series[i][j] = ASCII_sum
-
-
-    ###### check output folders
+    # check output folders
     script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
     # Get the parent directory of the current script's directory
     parent_directory = os.path.dirname(script_directory)
@@ -299,12 +255,15 @@ def read_netCDF_formatted_PSS_series(inputpath, outputpath):
         os.makedirs(outputpath)
 
     # Save the data in .npz format
-    output_filename=os.path.join(outputpath,'Profile_Summary_Score_list.npz')
-    np.savez(output_filename, PSS_series=PSS_series, txt=txt,meta_names=meta_names,filenames=filenames)
+    output_filename = os.path.join(outputpath, 'Profile_Summary_Score_list.npz')
+    # Convert the DataFrame to a dictionary of arrays
+    data_dict = {col: PSS_series[col].values for col in PSS_series.columns}
+    # Save the dictionary to an .npz file
+    np.savez(output_filename, **data_dict)
 
     print('*******************FINISHED***********************************\n')
-    print('The Profile Summary Score formatted file are output to current folder: '+outputpath+'\n')
-    print('The Profile Summary Score filename is: '+output_filename)
+    print('The Profile Summary Score formatted file are output to current folder: ' + outputpath + '\n')
+    print('The Profile Summary Score filename is: ' + output_filename)
 
 
 if __name__ == '__main__':
@@ -322,10 +281,10 @@ if __name__ == '__main__':
     # check Input/Output dir vaild
     isInputOK = validate_path(InputDir)
     isOutputOK = validate_path(OutputDir)
-    if(not (isInputOK and isOutputOK)):
+    if (not (isInputOK and isOutputOK)):
         print("The entered path is not valid. Please ensure the path is correct and try again.")
         raise Exception("Invalid InputDir or OutputDir!", InputDir, OutputDir)
-    
+
     PSS_summary_filename = OutputDir + "/Profile_Summary_Score_list.npz"
     iAct = 1
     if os.path.exists(PSS_summary_filename):
@@ -333,5 +292,5 @@ if __name__ == '__main__':
         print(iAct)
 
     if (iAct == 1 or iAct == '1'):
-        read_netCDF_formatted_PSS_series(InputDir, OutputDir)
+        read_netCDF_formatted_PSS_series_sub(InputDir, OutputDir)
         print("Profile_Summary_Score_list.npz Complete !")
